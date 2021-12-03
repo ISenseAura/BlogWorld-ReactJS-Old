0d2b6910bd75c93cbbe062481321a5260105cffa
Glitch (uncovered-bustling-jade) <none>
2021-12-03 12:08:42 +0000

    🌨🚧 Checkpoint ./src/components/BlogItem.js:758933/28

--- src/components/BlogItem.js
+++ src/components/BlogItem.js
@@ -45,7 +45,7 @@ const BlogItem = (props)=> {
                     }> 
                         <span className="badge rounded-pill bg-danger"> {!author ? "Unknown" : author} </span>
                     </div>
-                    <img src={!imageUrl ? "https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-annoucnements/-476x249w4/gsmarena_00.jpg" : imageUrl} className="card-img-top" alt="..." />
+                    <img width="100%" height="225" src={!imageUrl ? "https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-annoucnements/-476x249w4/gsmarena_00.jpg" : imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}  </h5>
                         <p className="card-text">{description}</p>

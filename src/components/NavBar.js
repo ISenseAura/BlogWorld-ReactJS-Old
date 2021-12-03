1166f7925542ab3c61d4d757a4d0e1a7d9ff18b6
Glitch (uncovered-bustling-jade) <none>
2021-12-03 06:24:25 +0000

    🎾🎷 Checkpoint ./src/components/NavBar.js:758933/1083

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -15,12 +15,17 @@ const NavBar = () => {
           <p className="text-muted">Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information.</p>
         </div>
         <div className="col-sm-4 offset-md-1 py-4">
-          <h4 className="text-white">Contact</h4>
-          <ul className="list-unstyled">
-            <li><a href="#" className="text-white">Follow on Twitter</a></li>
-            <li><a href="#" className="text-white">Like on Facebook</a></li>
-            <li><a href="#" className="text-white">Email me</a></li>
-          </ul>
+         <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link className="nav-link" aria-current="page" to="/">Home</Link>
+                            </li>
+                            <li className="nav-item"><Link className="nav-link" to="/myblogs">My Blogs</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/login">Login</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/signup">SignUp</Link></li>
+                      
+                        </ul>
+                    </div>
         </div>
       </div>
     </div>
@@ -29,7 +34,7 @@ const NavBar = () => {
     <div className="container">
       <a href="#" className="navbar-brand d-flex align-items-center">
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" className="me-2" viewBox="0 0 24 24"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"/><circle cx="12" cy="13" r="4"/></svg>
-        <strong>Album</strong>
+        <strong>BlogWorld</strong>
       </a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>

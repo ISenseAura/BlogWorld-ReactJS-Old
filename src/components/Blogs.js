5be0a6c9402cb6356588f38c8fff12f8910e12ab
Glitch (uncovered-bustling-jade) <none>
2021-12-03 06:47:20 +0000

    🏤🐛 Checkpoint ./src/components/NavBar.js:758933/289 ./src/components/Blogs.js:758933/1378

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -46,14 +46,27 @@ const Blogs = (props)=>{
  
         return (
             <>
-                <h1 className="text-center" style={{ margin: '35px 0px', marginTop: '90px' }}>Recent Blogs</h1>
-                {loading && <Spinner />}
+            
+                  <section className="py-5 text-center container">
+    <div className="row py-lg-5">
+      <div className="col-lg-6 col-md-8 mx-auto">
+        <h1 className="fw-light">Album example</h1>
+        <p className="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
+        <p>
+          <a href="#" className="btn btn-primary my-2 mx-2">Main call to action</a>
+          <a href="#" className="btn btn-secondary my-2 mx-2">Secondary action</a>
+        </p>
+      </div>
+    </div>
+  </section>
+                  {loading && <Spinner />}
                 <InfiniteScroll
                     dataLength={articles.length}
                     next={fetchMoreData}
                     hasMore={articles.length !== totalResults}
                     loader={<Spinner/>}
                 > 
+                  
                     <div className="container">
                          
                     <div className="row">
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,7 +12,7 @@ const NavBar = () => {
       <div className="row">
         <div className="col-sm-8 col-md-7 py-4">
           <h4 className="text-white">About</h4>
-          <p className="text-muted">Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information.</p>
+          <p className="text-muted">A Place for me and my friends to write blogs.</p>
         </div>
         
          <div className="col-sm-4 offset-md-1 py-4">

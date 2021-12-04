1b70a730977702c92130d52c82a5f53595fffeed
Glitch (uncovered-bustling-jade) <none>
2021-12-04 08:37:57 +0000

    🏇😜 Checkpoint ./src/components/BlogItem.js:758933/39

--- src/components/BlogItem.js
+++ src/components/BlogItem.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 const BlogItem = (props)=> {
-        let { title, description, imageUrl, author, date,id } = props;
+        let { title, description, imageUrl, author, date, id } = props;
   
   const deletePost = async () => {
+    alert(id);
     const response = await fetch("https://glistening-lackadaisical-glue.glitch.me/api/posts/delete/" + id, {
             method: 'GET',
             headers: {
@@ -12,7 +13,8 @@ const BlogItem = (props)=> {
             }
 
         });
-    let json = response.json()
+    let json = await response.json()
+    alert(json);
     if(json.success) {
       alert("Post deleted successfully")
       window.reload();

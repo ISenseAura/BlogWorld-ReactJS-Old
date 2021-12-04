cc665d138f03ac5bcbf4d0e2f9042c7f98023e5b
Glitch (uncovered-bustling-jade) <none>
2021-12-04 08:13:50 +0000

    🎨🎙 Checkpoint ./src/components/BlogItem.js:758933/1003

--- src/components/BlogItem.js
+++ src/components/BlogItem.js
@@ -1,7 +1,29 @@
 import React from 'react'
 
 const BlogItem = (props)=> {
-        let { title, description, imageUrl, author, date } = props;
+        let { title, description, imageUrl, author, date,id } = props;
+  
+  const deletePost = async (id) => {
+    const response = await fetch("https://glistening-lackadaisical-glue.glitch.me/api/posts/delete/" + id, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+              'auth-token' : localStorage.getItem("token")
+            }
+
+        });
+    let json = response.json()
+    if(json.success) {
+      alert("Post deleted successfully")
+      window.reload();
+    }
+    else {
+      alert("Access Denied")
+    }
+    
+  }
+  
+  
   /*
   return (
   <div className="card shadow-sm">
@@ -53,7 +75,9 @@ const BlogItem = (props)=> {
                                             <div className="btn-group">
                   <button type="button" className="btn btn-sm btn-outline-secondary">👍 31 <i class="bi-like"></i> </button>
                   <button type="button" className="btn btn-sm btn-outline-secondary">👎 21</button>
-                </div>
+                </div> <br></br>
+                        <button type="button" onClick={deletePost(id)}className="btn btn-sm btn-outline-secondary">DELETE</button>
+      
                 <small className="text-muted"> <small> {new Date(date).toGMTString()}</small></small>
                     </div>
                   </div>

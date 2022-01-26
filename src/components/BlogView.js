8fdfc574d046e001402f1f12a1c9b7c806f2faff
Glitch (uncovered-bustling-jade) <none>
2022-01-26 05:46:11 +0000

    🍂🏈 Checkpoint ./src/components/BlogView.js:758933/665

--- src/components/BlogView.js
+++ src/components/BlogView.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import { useParams } from "react-router-dom"
+import { useHistory } from 'react-router-dom'
+  
 
-const BlogView=  async (props)=> {
+const BlogView=   (props)=> {
         let { title} = props;
   let {tag} = useParams();
+  let post;
 
+  let history = useHistory();
+  
+  const getPost = async () => {
   const response = await fetch("https://glistening-lackadaisical-glue.glitch.me/api/posts/get/" + tag, {
             method: 'GET',
             headers: {
@@ -14,14 +20,22 @@ const BlogView=  async (props)=> {
 
         });
   
-  if()
+  if(response.success) {
+    post = response.post;
+  }
+  else {
+    alert("Something went wrong!");
+    history.push("/");
+  }
+  }
+  getPost();
   
         return (
           
             <div className="container my-3">
-            <h1> {tag} </h1>
+            <h1> {post.title} </h1>
             <figure class="figure">
-              <img src="https://cdn-icons-png.flaticon.com/512/919/919827.png" class="figure-img img-fluid rounded" alt="..."></img>
+              <img src="post.img" class="figure-img img-fluid rounded" alt="..."></img>
   <figcaption class="figure-caption"> {title} </figcaption>
 </figure>
             </div>

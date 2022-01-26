c93b03f65c4950508bb6dc2f45ab1b1b7e0d2e59
Glitch (uncovered-bustling-jade) <none>
2022-01-26 05:10:11 +0000

    🚒🏜 Checkpoint ./src/components/Blogs.js:758933/59 ./src/components/Post.js:758933/187

--- src/components/Post.js
+++ src/components/Post.js
@@ -49,7 +49,7 @@ const AddPost = (props) => {
         'Content-Type': 'application/json',
         "auth-token" : localStorage.getItem("token") //`eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiemVyYXBpdW0iLCJ1c2VybmFtZSI6IlplcmFwaXVtIiwiZW1haWwiOiJtbW1AbW0uY29tIn0sImlhdCI6MTYzODUyNDM0Nn0.eWm0y_ULVu57suXg7BhHwS1XKisqLd0ZQDMYCp7UPXo`
       },
-      body: JSON.stringify({'title' : note.title, 'text' : note.description, 'img' : note.img})
+      body: JSON.stringify({'postid' : note.postid.length ? note.postid.trim() : false, 'title' : note.title, 'text' : note.ldescription, 'img' : note.img, 'short' : note.sdescription.length ? note.sdescription : false})
     });
 
     const json = await response.json();
@@ -75,7 +75,7 @@ const AddPost = (props) => {
             <h2>Add a Blog</h2>
             <form className="my-3" onSubmit={addNote}>
        <div className="mb-3">
-                    <label htmlFor="postid" className="form-label">Post ID (Optional)</label> <br>
+                    <label htmlFor="postid" className="form-label">Post ID (Optional)</label> 
                       <small><i> (Enter post id if you wanna add to existing blog) </i> </small>
 
                     <input type="text" className="form-control" id="postid" name="postid" value={note.postid} onChange={onChange} minLength={5} required />
